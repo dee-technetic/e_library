f6292e96bbb884961eccbc45379e99841c08c00c
adedoyin adesewa <adedolapoadedoyin541@gmail.com>
2024-08-26 13:19:44 +0100

    seprate js page added

---
+++ index.js
@@ -0,0 +1,32 @@
+ // let length = 5;
+        // let breath = 4;
+        // let area = length * breath;
+        // document.getElementById("output").innerHTML = area;
+        
+        // function getArea(length,breath) {
+        //     let area = length * breath;
+        //     return area;
+        // }
+        // console.log(getArea(2,4));
+
+        // function getArea(length,breath) {
+        //     let area = length * breath;
+        //     return area;
+        // }
+        // document.getElementById("output1").innerHTML = getArea(4,4);
+
+        // let x = 10;
+        // x +=5;
+        // console.log(x)
+
+        let country ='nigerian';
+        let age = 18;
+        let complexion = 'dark';
+
+        if (country === "nigerian" && age >= 18 && complexion === "fair") {
+            alert('you are eligible to vote');
+        }else{
+            alert('you are not eligible to vote');
+        }
+
+        
